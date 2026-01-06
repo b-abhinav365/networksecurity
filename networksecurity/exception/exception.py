f3a1c393557fdc1f9b4e54d65e8e81c9da22fb8e
Abhinav <abhinavsarathibondugula@gmail.com>
2026-01-06 11:41:15 +0530

    Data ingestion complete

--- networksecurity/exception/exception.py
+++ networksecurity/exception/exception.py
@@ -1,17 +1,37 @@
+# import sys
+# from networksecurity.logging import logger
+
+# class NetworkSecurityException(Exception):
+#     def __init__(self,error_message,error_details:sys):
+#         self.error_message = error_message
+#         _,_,exc_tb = error_details.exc_info()
+
+#         self.lineno=exc_tb.tb_lineno
+#         self.file_name = exc_tb.tb_frame.f_code.co_filename
+
+#     def __str__(self):
+#         return "Error occured in python script name [{0}] line number [{1} error message [{2}]".format(
+#         self.file_name, self.lineno, str(self.error_message))
+
+
+
 import sys
-from networksecurity.logging import logger
 
 class NetworkSecurityException(Exception):
-    def __init__(self,error_message,error_details:sys):
+    def __init__(self, error_message, error_details: sys):
         self.error_message = error_message
-        _,_,exc_tb = error_details.exc_info()
 
-        self.lineno=exc_tb.tb_lineno
-        self.file_name = exc_tb.tb_frame.f_code.co_filename
+        _, _, exc_tb = error_details.exc_info()
+
+        if exc_tb is not None:
+            self.lineno = exc_tb.tb_lineno
+            self.file_name = exc_tb.tb_frame.f_code.co_filename
+        else:
+            self.lineno = -1
+            self.file_name = "Unknown File"
 
     def __str__(self):
-        return "Error occured in python script name [{0}] line number [{1} error message [{2}]".format(
-        self.file_name, self.lineno, str(self.error_message))
-
-
-
+        return (
+            "Error occured in python script name [{0}] "
+            "line number [{1}] error message [{2}]"
+        ).format(self.file_name, self.lineno, str(self.error_message))
